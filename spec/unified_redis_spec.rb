6b29a218208339d017d260290ce6a95c147f47d8
Bruno Michel <bmichel@menfin.info>
2011-10-24 23:36:45 +0200

    Really share the specs between the two adapters

--- spec/unified_redis_spec.rb
+++ spec/unified_redis_spec.rb
@@ -1,59 +1,47 @@
 require 'spec_helper'
 
-shared_examples "unified_redis_sync" do
-  before do
-    @r = UnifiedRedis.new(redis)
-    @r.del("chuck")
-  end
+
+shared_examples "unified_redis" do
 
   it "should be able to get/set" do
-    @r.get("chuck") do |value|
-      value.should == nil
-    end
-    @r.set("chuck", "norris")
-    @r.get("chuck") do |value|
-      value.should == 'norris'
-    end
-  end
-end
-
-shared_examples "unified_redis_async" do
-  around(:each) do |example|
-    EM.run do
-      @r = UnifiedRedis.new(redis)
-      @r.del("chuck", &example)
-    end
-  end
-
-  it "should be able to get/set" do
-    @r.get("chuck") do |values|
-      values.should == nil
-      @r.set("chuck", "norris") do
-        @r.get("chuck") do |value|
-          value.should == 'norris'
-          EM.stop
+    @r.del("chuck") do
+      @r.get("chuck") do |value|
+        value.should == nil
+        @r.set("chuck", "norris") do
+          @r.get("chuck") do |value|
+            value.should == 'norris'
+            EM.stop if EM.reactor_running?
+          end
         end
       end
     end
   end
+
 end
+
 
 describe "Unified::Redis" do
   context "with redis-rb" do
-    it_should_behave_like "unified_redis_sync" do
-      let (:redis) { Redis.new }
+    before do
+      @r = UnifiedRedis.new(Redis.new)
     end
+
+    it_should_behave_like "unified_redis"
   end
 
+
   context "with em-redis" do
-    it_should_behave_like "unified_redis_async" do
-      let (:redis) {
-        r = EM::Protocols::Redis.connect
-        r.errback do |code|
+    around(:each) do |example|
+      EM.run do
+        redis = EM::Protocols::Redis.connect
+        redis.errback do |code|
           puts "redis error"
         end
-        r
-      }
+        @r = UnifiedRedis.new(redis)
+        example.call
+      end
     end
+
+    it_should_behave_like "unified_redis"
   end
 end
