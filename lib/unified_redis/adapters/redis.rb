12dbfe9abe1ceeebacbabf1a138523c3188dceb1
chatgris <jboyer@af83.com>
2011-10-25 15:35:24 +0200

    replace method_missing by define_method for redis-rb

--- lib/unified_redis/adapters/redis.rb
+++ lib/unified_redis/adapters/redis.rb
@@ -3,13 +3,15 @@ module UnifiedRedis
   module Adapter
     class Redis
       def initialize(redis)
-        @redis = redis
-      end
-
-      def method_missing(command, *args)
-        result = @redis.send(command, *args)
-        yield result if block_given?
-        return result
+        redis.class.instance_methods(false).each do |command|
+          self.class.instance_eval do
+            define_method command do |*args|
+              result = redis.send(command, *args)
+              yield result if block_given?
+              return result
+            end
+          end
+        end
       end
     end
   end
