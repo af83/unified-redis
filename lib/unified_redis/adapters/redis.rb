184aac836927f2de4eccce3ad9d801f09f8da760
chatgris <jboyer@af83.com>
2011-10-25 18:45:27 +0200

    create methods at class load

--- lib/unified_redis/adapters/redis.rb
+++ lib/unified_redis/adapters/redis.rb
@@ -3,14 +3,14 @@ module UnifiedRedis
   module Adapter
     class Redis
       def initialize(redis)
-        redis.class.instance_methods(false).each do |command|
-          self.class.instance_eval do
-            define_method command do |*args|
-              result = redis.send(command, *args)
-              yield result if block_given?
-              return result
-            end
-          end
+        @redis = redis
+      end
+
+      ::Redis.instance_methods(false).each do |command|
+        define_method command do |*args|
+          result = @redis.send(command, *args)
+          yield result if block_given?
+          return result
         end
       end
     end
